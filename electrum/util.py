0bbeec3b15541bfcd3579e96248378859d1b775c
Borland04 <McGarret160497@gmail.com>
2021-01-15 22:33:04 +0300

    Add 'oPoW' block explorer config. Use it as default block explorer

--- electrum/util.py
+++ electrum/util.py
@@ -777,6 +777,8 @@ mainnet_block_explorers = {
                         {'tx': 'tx/', 'addr': 'address/'}),
     'system default': ('blockchain:/',
                         {'tx': 'tx/', 'addr': 'address/'}),
+    'oPoW': ('http://46.4.98.180:81/',
+                        {'tx': 'tx/', 'addr': 'address/'}),
 }
 
 testnet_block_explorers = {
@@ -802,7 +804,7 @@ def block_explorer_info():
 
 def block_explorer(config: 'SimpleConfig') -> str:
     from . import constants
-    default_ = 'Blockstream.info'
+    default_ = 'oPoW'
     be_key = config.get('block_explorer', default_)
     be = block_explorer_info().get(be_key)
     return be_key if be is not None else default_
