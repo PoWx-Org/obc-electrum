1c641d7d0ddd6538a25e522e2c76ced82d5400e4
Borland04 <McGarret160497@gmail.com>
2021-01-21 21:19:15 +0300

    Change currency units: 'BTC' -> 'OBTC'

--- electrum/util.py
+++ electrum/util.py
@@ -73,9 +73,9 @@ def inv_dict(d):
 ca_path = certifi.where()
 
 
-base_units = {'BTC':8, 'mBTC':5, 'bits':2, 'sat':0}
+base_units = {'OBTC':8, 'mOBTC':5, 'bits':2, 'sat':0}
 base_units_inverse = inv_dict(base_units)
-base_units_list = ['BTC', 'mBTC', 'bits', 'sat']  # list(dict) does not guarantee order
+base_units_list = ['OBTC', 'mOBTC', 'bits', 'sat']  # list(dict) does not guarantee order
 
 DECIMAL_POINT_DEFAULT = 5  # mBTC
 
